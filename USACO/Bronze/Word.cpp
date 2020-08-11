bd240f336526158265dd413b05a6d0a99298245e
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2020-08-11 09:44:27 -0700

    Create Word.cpp

---
+++ USACO/Bronze/Word.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+#include <map>
+#include <fstream>
+using namespace std;
+int main(){
+    freopen("word.in", "r", stdin);
+    freopen("word.out", "w", stdout);
+    long long numofwords;
+    long long index;
+    cin >> numofwords;
+    cin >> index;
+    long long counter = 0;
+    for (int e = 0; e < numofwords; e++) {
+            string actuallmente;
+            cin >> actuallmente;
+            if (counter + actuallmente.size() > index) {
+                cout << endl;
+                cout << actuallmente;
+                counter = actuallmente.size();
+            } else {
+                if (e>0) cout << " ";
+                cout << actuallmente;
+                counter = counter+actuallmente.size();
+            }
+        }
+        cout << endl;
+    return 0;
+    }
