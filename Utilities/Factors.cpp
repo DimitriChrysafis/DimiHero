6a32207410f186d0871e8c7eccfbe4db1885291d
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2020-08-11 17:13:41 -0700

    Update Factors.cpp

--- Utilities/Factors.cpp
+++ Utilities/Factors.cpp
@@ -7,20 +7,46 @@
 #include <map>
 using namespace std;
 int main(){
-    
+    char pp;
+    cout << "Type p if you want the prime factors, and press f to see all the factors." << endl;
+    cin >> pp;
+    if(pp == 'f'){
     long long n, i;
 
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    long long counter = 1;
-    cout << "Factors of " << n << " are: " << endl;
-    for(i = 1; i <= n; ++i){
-        if(n % i == 0){
-              this_thread::sleep_for(chrono::milliseconds(10) );
-            cout << "Factor number " << counter << " is "<< i << endl;
-            counter++;
-        }
+       cout << "Enter a positive integer: ";
+       cin >> n;
+       long long counter = 0;
+       cout << "Factors of " << n << " are: " << endl;
+       for(i = 1; i <= n; ++i){
+           if(n % i == 0){
+                 this_thread::sleep_for(chrono::milliseconds(10) );
+               cout << "Factor number " << counter << " is "<< i << endl;
+               counter++;
+           }
+       }
+    cout << "The Numnber Of Factors is: " << counter << endl;
+    }else{
+    long long Number, p = 1, j, d, c = 0;
+      cout << "Enter a positive integer: " << endl;
+        cin >> Number;
+       while (p<=Number){
+           d = 0;
+           if(Number % p == 0){
+               j = 1;
+               while(j <= p){
+                   if(p % j == 0){
+                       d++;
+                   }
+                   j++;
+               }
+               if(d == 2){
+                   cout << "A prime factor is " << p << endl;
+                   c++;
+               }
+           }
+           p++;
+       }
+    cout << "Total number of prime factors is " << c << endl;
     }
- cout << "The Numnber Of Factors is:" << counter  -1<< endl;
-    return 0;
+       return 0;
 }
