3a6bd8c0dc177b503906e21520dfc4bd2c20a064
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2020-08-14 10:25:12 -0700

    Create FillingDiamonds.cpp

---
+++ CodeForces/900/FillingDiamonds.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+using namespace std;
+int main(){
+   int T;
+    cin >> T;
+   while(T--){
+       int n;
+       cin >> n;
+       cout << n << endl;
+   }
+   return 0;
+}
