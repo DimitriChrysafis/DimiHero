1a23bd1d064e2f85bf3dc0c846a7c2c27acd3e52
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2020-08-16 10:39:46 -0700

    Create MaxGCD.cpp

---
+++ CodeForces/800/MaxGCD.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+#include <map>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main(){
+  int l;
+    cin >> l;
+    while(l--){
+       int n;
+       cin >> n;
+       cout<< n / 2 <<endl;
+    }
+
+}
