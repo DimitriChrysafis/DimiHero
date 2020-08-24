980ac06d9ca2ea1da6e2f71946d853d3a06ccb63
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2020-08-24 09:31:52 -0700

    Create CountingBlockCombinations1.cpp

---
+++ ProjectEuler/CountingBlockCombinations1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#define ll long long int
+using namespace std;
+int oatmeal(){
+    
+    const ll rub=50;
+    ll sal[rub+1]={0};
+
+    sal[0]=1;
+    sal[1]=1;
+    sal[2]=1;
+    for(int n =  3; n <= rub ; n++){
+        sal[ n ] = 1 + sal [ n - 1 ] ;
+        for(int m = 3 ; m <= n - 1; m++){
+            sal[n] += sal[n-1-m];
+            
+        }
+        
+    }
+    cout << sal[rub] <<endl;
+    return sal[rub];
+}
+
+int main() {
+    oatmeal();
+}
