e13ca24c0c997026fa89c68b1940ec9df36c193a
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2020-09-01 13:28:09 -0700

    Update PrimeFactorization.cpp

--- Utilities/PrimeFactorization.cpp
+++ Utilities/PrimeFactorization.cpp
@@ -7,31 +7,34 @@
 #include <unistd.h>
 #include <chrono>
 #include <thread>
+#include <vector>
 #include <map>
 #include <algorithm>
 #include <cmath>
 #include <math.h>
 #define ll long long int
+#define pb push_back
 using namespace std;
+vector<int> v;
 void prm(ll n) {
    while (n % 2 == 0){
-       cout << 2 << "*";
+       v.pb(2);
       n = n/2;
    }
    for (int i = 3; i <= sqrt(n); i = i+2){
       while (n%i == 0){
-         cout << i << "*";
+          v.pb(i);
          n = n/i;
       }
    }
-    if (n > 2){
-        cout << n;
-    }
-    cout << endl;
+    if(n > 2) v.pb(n);
 }
 int main(){
    ll n;
     cin >> n;
-   prm(n);
+    prm(n);
+    for(int i = 0; i < v.size() - 1; i++){
+        cout << v[i] << "*";
+    }
+    cout << v[v.size() - 1];
 }
-
